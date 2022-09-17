086822eb357cbba6ee5c6759ce7f56b9cab31fee
Stefan Turner <stefanturner@MacBook-Air-von-Stefan.local>
2022-09-17 05:24:40 +0200

    setup new projectbox

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,11 @@ import { scroll } from "./projects/scroll";
 import { wheel } from "./projects/wheel";
 import { gradient } from "./projects/gradient";
 import { animal } from "./projects/animal";
+import { digitalclock } from "./projects/digitalclock";
 
 header();
 scroll();
 wheel();
 gradient();
 animal();
+digitalclock();
